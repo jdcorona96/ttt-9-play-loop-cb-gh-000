2caa00e5684c4de983c78984f34dabcae2a7ecda
Joseph Corona <josed.hc96@gmail.com>
2018-11-03 21:20:35 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -36,3 +36,15 @@ def turn(board)
 end
 
 # Define your play method below
+
+def play(board)
+
+  counter = 0
+  player1_playing = true
+
+  while (counter < 9) do
+
+    turn(board)
+    counter += 1
+  end
+end
